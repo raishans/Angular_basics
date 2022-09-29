641779746c4ced7372e0a0e1729ed6dd7e3db0db
Shanskar <raishanskar@mail.com>
2022-09-29 18:48:37 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My-App';
+
+  sum=0;
+  count = 5;
+  number1 = 0;
+  number2 = 0;
+  res = () => {
+    this.sum=this.number1+this.number2;
+  }
+  Increment = () => {
+    this.count++;
+  }
+  Decrement = () => {
+    this.count--;
+  }
+
+  list = [12, 13, 14, 15, 16, 17];
+  list2 = ["abc", "def", "ghi", "jkh"]
 }
